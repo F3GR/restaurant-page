fa0a503461e25f6b9a84483a74781d3177071593
F3GR <egmver.2@gmail.com>
2023-07-10 20:50:24 +0400

    finished with the site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,7 @@
-export function renderAboutPage() {
+import { createElementWithAttributes } from './utils.js';
 
+export function renderFrame() {
     const restaurantName = 'PAH ZAZ';
-    const paraOneText = 'Welcome to PAH ZAZ, the ultimate experience of the italian cuisine! We are dedicated to serving mouthwatering pizzas that will delight your taste buds and leave you craving for more.';
-    const paraTwoText = `At PAH ZAZ, we believe that pizza is not just a food; it's an art form, and we strive to create culinary masterpieces that will satisfy even the most discerning connoisseurs.`;
     const footerText = `&#169; PAH ZAZ, 2023`;
 
     const mainContent = document.querySelector('#content');
@@ -17,43 +16,28 @@ export function renderAboutPage() {
     const buttonMenu = createElementWithAttributes('button', { class: 'menu' }, nav);
     buttonMenu.textContent = 'Menu';
     const buttonContact = createElementWithAttributes('button', { class: 'contact' }, nav);
-    buttonContact.textContent = 'About';
+    buttonContact.textContent = 'Contact';
 
     const main = createElementWithAttributes('main', {}, mainContent);
-    const paraOne = createElementWithAttributes('p', { class: 'one' }, main);
-    paraOne.textContent = paraOneText;
-    const paraTwo = createElementWithAttributes('p', { class: 'two' }, main);
-    paraTwo.textContent = paraTwoText;
-
     const footer = createElementWithAttributes('footer', {}, mainContent);
     footer.textContent = footerText;
 }
 
-function createElementWithAttributes(tagName, attributes = {}, parentElement) {
-    const element = document.createElement(tagName);
-  
-    Object.entries(attributes).forEach(([key, value]) => {
-      element.setAttribute(key, value);
-    });
-  
-    if (parentElement) {
-      parentElement.appendChild(element);
-    }
-  
-    return element;
-  }
 
+export function renderAboutPage() {
+    const paraOneText = 'Welcome to PAH ZAZ, the ultimate experience of the italian cuisine! We are dedicated to serving mouthwatering pizzas that will delight your taste buds and leave you craving for more.';
+    const paraTwoText = `At PAH ZAZ, we believe that pizza is not just a food; it's an art form, and we strive to create culinary masterpieces that will satisfy even the most discerning connoisseurs.`;
 
-/* h1 PAH ZAZ
+    const selectedMain = document.querySelector('main');
+    selectedMain.innerHTML = "";
+    
+    const selectedButton = document.querySelector('button.about');
+    selectedButton.setAttribute('data-value', 'current');
 
-<nav>
-<button class="about">About</button>
-<button class="menu">Menu</button>
-<button class="contact">Contact</button>
-</nav>
-<main>
-<p>Welcome to PAH ZAZ, the ultimate experience of the italian cuisine! We are dedicated to serving mouthwatering pizzas that will delight your taste buds and leave you craving for more. </p>
-<p>At PAH ZAZ, we believe that pizza is not just a food; it's an art form, and we strive to create culinary masterpieces that will satisfy even the most discerning connoisseurs.</p>
-</main>
-<footer>&#169; PAH ZAZ, 2023</footer> */
+    const paraOne = createElementWithAttributes('p', { class: 'one' }, selectedMain);
+    paraOne.textContent = paraOneText;
+    const paraTwo = createElementWithAttributes('p', { class: 'two' }, selectedMain);
+    paraTwo.textContent = paraTwoText;
 
+    createElementWithAttributes('img', { class: 'restaurant-photo', src: '../src/originals/pah-zaz.jpeg', alt: `Restaurant's photo` }, selectedMain);
+}
